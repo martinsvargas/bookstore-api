3389b8ed47ad3c3cb175075b158917e706368eef
Alexandre Vargas <alexandremv@gmail.com>
2021-07-19 08:51:41 -0300

    Classes de domínio iniciadas

---
+++ src/main/java/br/com/martinsvargas/bookstore/domain/Livro.java
@@ -0,0 +1,11 @@
+package br.com.martinsvargas.bookstore.domain;
+
+public class Livro {
+	private Integer id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+	
+	private Categoria categoria;
+
+}
